c54f703e3c6b041a6bdf96aa6680e0a07e89c8f5
gareth-go <gareth.le.goldenowl@gmail.com>
2023-08-18 11:50:32 +0700

    remove comment seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ActiveRecord::Base.transaction do
-  # 100.times.each do |time|
-  #   # Tag.create!(name: Faker::Lorem.unique.word)
-  #   User.create!(username: "#{Faker::Artist.name} #{time}", email: Faker::Internet.unique.email, password: "123456")
-  # end
+  100.times.each do |time|
+    # Tag.create!(name: Faker::Lorem.unique.word)
+    User.create!(username: "#{Faker::Artist.name} #{time}", email: Faker::Internet.unique.email, password: "123456")
+  end
 
-  # tags = Tag.all
-  # users = User.all
+  tags = Tag.all
+  users = User.all
 
-  # 100.times.each do
-  #   Post.create!(title: Faker::Lorem.unique.sentence, content: Faker::Lorem.paragraphs, user: users.sample, tags: tags.sample(4))
-  # end
+  100.times.each do
+    Post.create!(title: Faker::Lorem.unique.sentence, content: Faker::Lorem.paragraphs, user: users.sample, tags: tags.sample(4))
+  end
 end
