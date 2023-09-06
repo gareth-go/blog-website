789c0f446c1085c5bca14cbc921298070b211f6e
gareth-go <gareth.le.goldenowl@gmail.com>
2023-09-06 16:56:30 +0700

    refactor posts service

--- app/services/posts/list_posts_service.rb
+++ app/services/posts/list_posts_service.rb
@@ -1,5 +1,5 @@
 module Posts
-  class PostsFilterService < ApplicationService
+  class ListPostsService < ApplicationService
     def initialize(posts, params)
       @posts = posts
       @params = params
@@ -8,6 +8,7 @@ module Posts
     def call
       search if @params[:search].present?
       filter_by_status if Post.statuses.include?(@params[:status])
+      sort
 
       @posts
     end
@@ -22,6 +23,17 @@ module Posts
 
     def filter_by_status
       @posts = @posts.where(status: @params[:status])
+    end
+
+    def sort
+      case @params[:sort]
+      when 'comments'
+        @posts = @posts.order(comments_count: :desc)
+      when 'reactions'
+        @posts = @posts.order(reactions_count: :desc)
+      end
+
+      @posts = @posts.order(created_at: :desc)
     end
   end
 end
