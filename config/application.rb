b09b54375be828119f1df8e85fa516975aac71b3
gareth-go <gareth.le.goldenowl@gmail.com>
2023-10-05 17:54:42 +0700

    config env load

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BlogWebsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
+    Dotenv::Railtie.load
 
     # Configuration for the application, engines, and railties goes here.
     #
