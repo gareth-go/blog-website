2554ef8892ca2a5fb66eb39b1302a079de9cce58
gareth-go <gareth.le.goldenowl@gmail.com>
2023-10-11 09:11:09 +0700

    remove redundant space

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -2,9 +2,9 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("NotificationsChannel", {
   received(data) {
-    document.querySelector('body').insertAdjacentHTML( "beforeend", data )
+    document.querySelector("body").insertAdjacentHTML("beforeend",data)
     
-    document.querySelector('#unview-notifications-count').innerHTML = +document.querySelector('#unview-notifications-count').innerHTML + 1
+    document.querySelector("#unview-notifications-count").innerHTML = +document.querySelector("#unview-notifications-count").innerHTML + 1
 
     setTimeout(function() {
       $(".alert").alert("close");
